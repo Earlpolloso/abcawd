aef5bff9fbf03d36c3aed50c966cfdf78fbfc137
Earlpolloso <earlpolloso@gmail.com>
2024-07-17 04:23:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-//JotaroKujo0525 note, this is a deed that i should've done a long time ago
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <html>
+      <head>
+        <title>Your Web View</title>
+      </head>
+      <body style="margin: 0; padding: 0;">
+        <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+      </body>
+    </html>`);
+});
+
+server.listen(3000, () => {
+  console.log('Server Online because of Axo Coder ✅!!');
+});
 require('dotenv').config()
 
 const DiscordMusicBot = require("./lib/DiscordMusicBot");
